254b03c37e7d02cf3c982b85e4e581cb7bbde6c0
helen3141 <helchoi@microsoft.com>
2020-08-28 14:05:33 -0700

    screenshots

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,10 +1,12 @@
+var webdriver = require('selenium-webdriver');
+var chromeDriver = require('selenium-webdriver/chrome');
 // @ts-ignore
 jest.retryTimes(3);
 // retry 
 describe('Get results', function () {
-    // test('Should Always Pass', () => {
-    //     expect(1).toBe(1);
-    // });
+    test('Should Always Pass', () => {
+        expect(1).toBe(1);
+    });
 
     test('Should Sometimes Pass', () => {
         const someNum = Math.floor(Math.random() * 10);
@@ -12,7 +14,31 @@ describe('Get results', function () {
         expect(someNum).toBeGreaterThan(10);
     });
 
-    // test('Should Always Fail', () => {
-    //     expect(2).toBe(1);
-    // });
+    test('Should Always Fail', () => {
+        expect(2).toBe(1);
+    });
+
+    test('Screenshot Chrome', () => {
+        var driver = new webdriver.Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new chromeDriver.Options()
+        .addArguments("window-size=1920,1080"))
+        .build();
+    
+        driver.get('https://www.bing.com/search?q=test');
+        
+        driver.takeScreenshot().then(
+            function(image, err) {
+                require('fs').writeFile('Screenshots/1.png', image, 'base64', function(err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('success!!!')
+                    }
+                });
+            }
+        );
+        
+    driver.quit();
+    });
 });
