29aec1720523ba71b136bb39e39dea6c718f68db
helen3141 <helchoi@microsoft.com>
2020-08-28 14:22:46 -0700

    screenshots

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -4,19 +4,19 @@ var chromeDriver = require('selenium-webdriver/chrome');
 jest.retryTimes(3);
 // retry 
 describe('Get results', function () {
-    test('Should Always Pass', () => {
-        expect(1).toBe(1);
-    });
+    // test('Should Always Pass', () => {
+    //     expect(1).toBe(1);
+    // });
 
-    test('Should Sometimes Pass', () => {
-        const someNum = Math.floor(Math.random() * 10);
-        console.log('randomly chosen number: ' + someNum);
-        expect(someNum).toBeGreaterThan(10);
-    });
+    // test('Should Sometimes Pass', () => {
+    //     const someNum = Math.floor(Math.random() * 10);
+    //     console.log('randomly chosen number: ' + someNum);
+    //     expect(someNum).toBeGreaterThan(10);
+    // });
 
-    test('Should Always Fail', () => {
-        expect(2).toBe(1);
-    });
+    // test('Should Always Fail', () => {
+    //     expect(2).toBe(1);
+    // });
 
     test('Screenshot Chrome', () => {
         var driver = new webdriver.Builder()
@@ -29,7 +29,7 @@ describe('Get results', function () {
         
         driver.takeScreenshot().then(
             function(image, err) {
-                require('fs').writeFile('Screenshots/1.png', image, 'base64', function(err) {
+                require('fs').writeFile('1.png', image, 'base64', function(err) {
                     if (err) {
                         console.log(err);
                     } else {
